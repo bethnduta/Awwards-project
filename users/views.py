3b70a93f595c7aed562b4836fabdc4820ab887fc
beth nduta <bethnduta05@gmail.com>
2022-06-13 05:55:05 +0300

    register views

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.isvalid():
+            username=form.cleaned_data['username']
+            messages.success(request,f'Account created for {username}!')
     form = UserCreationForm()
     return render(request, 'users/register.html',{'form':form})
