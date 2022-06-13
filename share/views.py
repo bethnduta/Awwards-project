4fb5b0e6d51de547475df9e2a1849014305744f6
beth nduta <bethnduta05@gmail.com>
2022-06-13 05:45:05 +0300

    register page

--- share/views.py
+++ share/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 posts=[
@@ -17,7 +18,7 @@ posts=[
     }
 ]
 def home(request):
-    context={'posts':posts}
+    context={'posts':Post.objects.all()}
     return render(request, 'share/home.html',context)
     
 def about(request):
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
-from share.models import Post
-
 # Register your models here.
-admin.site.register(Post)
