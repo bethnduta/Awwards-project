6c3daf368e7bafb54bf913af537a5d31c3cefb53
beth nduta <bethnduta05@gmail.com>
2022-06-17 00:02:56 +0300

    deployment

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+@login_required
 
 
 # Create your views here.
