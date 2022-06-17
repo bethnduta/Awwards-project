68f01c090e78b55ba00c3bf70ab8bfa4979ed5ef
beth nduta <bethnduta05@gmail.com>
2022-06-17 13:46:36 +0300

    deployment heroku

--- users/models.py
+++ users/models.py
@@ -4,17 +4,9 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-
-    def update_profile_(self, new_profile):
-        self.profile = new_profile
-        self.save()
+    image = models.ImageField(default='flower.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} Profile'
+
+    
