7ff945a5db7032e031123e53e55d2e963ad5f29c
beth nduta <bethnduta05@gmail.com>
2022-06-17 17:51:59 +0300

    deploy to heroku

--- share/models.py
+++ share/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Post(models.Model):
